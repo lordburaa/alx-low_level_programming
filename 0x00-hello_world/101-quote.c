53e0deb37d4a773b71d365ce6ed7a363fbccb94a
lordburaa <comrdebiruk97@gmail.com>
2023-07-14 16:17:33 -0700

    101-qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - printf that piece of art is useful \" -dora korpar,
+ * no printf and puts
+ * Return: Always 1
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19", 59);
+return (1);
+}
