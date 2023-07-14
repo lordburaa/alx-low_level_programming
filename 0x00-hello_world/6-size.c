418578aade281d6134a7859b39bf66c4997a5118
lordburaa <comrdebiruk97@gmail.com>
2023-07-14 06:40:11 -0700

    6-size.c updated7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ long sL;
 long long sLL;
 float sF;
 printf("Size of a char: %lu byte(s)\n", sizeof(sC));
-printf("Size of an char: %lu byte(s)\n", sizeof(sI));
+printf("Size of an int: %lu byte(s)\n", sizeof(sI));
 printf("Size of a long int: %lu byte(s)\n", sizeof(sL));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(sLL));
 printf("Size of a float: %lu byte(s)\n", sizeof(sF));
