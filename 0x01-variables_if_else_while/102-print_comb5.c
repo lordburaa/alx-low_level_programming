b437d4976930fc2d8519f007e1404077f5ce40d2
lordburaa <comrdebiruk97@gmail.com>
2023-07-16 10:55:12 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - pair of three digit without repetition
+ * Return: Always 0
+ */
+int main(void)
+{
+int e, f;
+	for (e = 0; e <= 99; e++)
+	{
+		for (f = 0; f <= 99; f++)
+		{
+			if (e < f && e != f)
+			{
+				putchar((e / 10) + '0');
+				putchar((f % 10) + '0');
+				putchar((" "));
+				putchar((f / 10) + '0');
+				putchar((f % 10) + '0');
+				if (e != 98 || f != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
