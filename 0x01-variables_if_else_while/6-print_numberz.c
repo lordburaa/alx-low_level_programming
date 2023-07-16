b395fe33d945963ad529353a4df5a95dae61afcc
lordburaa <comrdebiruk97@gmail.com>
2023-07-16 01:09:06 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - print 
+ * Return: Always 0
+ */
+int main(void)
+{
+int i, num;
+for (i = 0; i < 10; i++)
+{
+num = i;
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
