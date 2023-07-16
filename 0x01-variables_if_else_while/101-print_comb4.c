89d1a07f6af837faddf63ca658c8b5d998c8db4f
lordburaa <comrdebiruk97@gmail.com>
2023-07-16 09:57:32 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+ * main - print three combination of number
+ * Return: Always 0
+ */
+int main(void)
+{
+int i, j, k;
+i = '0';
+j = '0';
+k = '0';
+for (i = '0'; i < '9'; i++)
+{
+	for (j = '1'; j < '9'; j++)
+	{
+		for (k = '0'; k <= '9' ; k++)
+		{
+			if (j > i && j < k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if ((k == '9' && j == '8') && i == '7')
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+}
+putchar('\n');
+return(0);
+}
