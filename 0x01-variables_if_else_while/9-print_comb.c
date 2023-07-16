c2d89e25670d731c68c7ff008d9cd89101f692da
lordburaa <comrdebiruk97@gmail.com>
2023-07-16 02:14:35 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - print number separated by comma and space
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i <= 9; i++)
+{
+putchar(i + 48);
+if (i != 9)
+{
+putchar(44);
+}
+putchar(31);
+}
+putchar('\n');
+return (0);
+}
