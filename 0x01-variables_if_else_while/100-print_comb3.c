e229ac2f2e6b103dfd80583e305e3c46c64054ee
lordburaa <comrdebiruk97@gmail.com>
2023-07-16 07:04:11 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/**
+ * main - combination of number
+ * Return: Always 0
+ */
 int main(void)
 {
 int i, j;
 i = '0';
 j = '0';
-for (i = '0'; i < '9'; i++) 
+for (i = '0'; i < '9'; i++)
 {
 	for (j = '1'; j <= '9'; j++)
 	{
-		if( j <= '9')
+		if (j <= '9' && (i != j))
 		{
-		
-		
-		putchar(i);
-		putchar(j);
-		putchar(',');
-		putchar(' ');
+			putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
 		}
 		else
 			continue;
@@ -26,4 +27,5 @@ for (i = '0'; i < '9'; i++)
 
 }
 putchar('\n');
-return (0);}
+return (0);
+}
