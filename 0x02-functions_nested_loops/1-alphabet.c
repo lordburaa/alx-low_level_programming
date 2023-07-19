c3fecd28a346b75a9b7893c2e05c527de25d4b5a
lordburaa <comrdebiruk97@gmail.com>
2023-07-19 00:08:16 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  * lower case
  * Return: Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
-	int i;
+	char i;
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
+	
 }
