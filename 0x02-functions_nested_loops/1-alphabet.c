634fc432363fcc5d597efa180f6e216886e3f398
lordburaa <comrdebiruk97@gmail.com>
2023-07-18 23:55:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - alphabet using putchar
+ * lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
