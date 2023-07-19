45ee81ba28ae7ed0ba44f0c032cd60afc99c70b6
lordburaa <comrdebiruk97@gmail.com>
2023-07-19 13:21:46 -0700

    104-fibonacci.c updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int count;
 	unsigned long fb1 = 0, fb2 = 1, sum;
-	unsigned long fb1_half1, fb1_half2,fb2_half1, fb2_half2;
+	unsigned long fb1_half1, fb1_half2, fb2_half1, fb2_half2;
 	unsigned long half1, half2;
 
 	for (count = 0; count < 92; count++)
