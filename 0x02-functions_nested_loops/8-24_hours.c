55e48e4f0684275e6039d7ee1ebafc7cf3cb0afc
lordburaa <comrdebiruk97@gmail.com>
2023-07-19 04:41:24 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,20 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	i = 0;
 	while (i < 24)
 	{
 		j = 0;
-		while(j < 60)
+		while (j < 60)
 		{
-
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
+		i++;
 	}
 }
