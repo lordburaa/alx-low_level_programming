8336772f61e11c5229237ca975f3a2480f4e33b5
lordburaa <comrdebiruk97@gmail.com>
2023-07-19 04:54:26 -0700

    11-print_to_98.c7

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 /**
  * print_to_98 - print all natural number
+ * @n: int to be checked
  * Return: Always 0
  */
 void print_to_98(int n)
 {
 	int i, j;
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
