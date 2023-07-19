8e9194e11f4d4c8945c50595929c30391ba2d590
lordburaa <comrdebiruk97@gmail.com>
2023-07-19 13:57:19 -0700

    5-sign.c updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 0)
@@ -23,5 +23,5 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	_putchar('\n\);
+	_putchar('\n');
 }
