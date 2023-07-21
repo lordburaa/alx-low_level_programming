b1806f37320acc1b5bd2adb15d48e28f4364d2da
lordburaa <comrdebiruk97@gmail.com>
2023-07-21 08:07:08 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - prime factor of the numberr
+ * Retrun: Always 0
+ */
+int main(void)
+{
+	long x, maxF;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x =1; x <=  square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxF = number /x;
+		}
+	}
+	printf("%ld\n", maxF);
+	return (0);
+}
