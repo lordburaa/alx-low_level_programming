2a95b9c13d532d5acb20a0e8a8aa8337163e9320
lordburaa <comrdebiruk97@gmail.com>
2023-07-21 06:50:52 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x 5 5 ==0))
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
-		else if (x % 3 ==0)
+		else if (x % 3 == 0)
 		{
 			printf("Fizz");
 		}
