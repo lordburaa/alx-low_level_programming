c95599ca35ed80fe183284712f6461307837b25b
lordburaa <comrdebiruk97@gmail.com>
2023-07-24 04:55:16 -0700

    0-reset_to_98.c2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
-#include <unistd.h>
-int _putchar(char a)
-{
-	return (write(1,&a,1));
-}
+
+/**
+ * reset_to_98 - print value
+ * ret - return the value
+ * @n: int to be checked
+ * Return: Always 0
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
- 
 }
