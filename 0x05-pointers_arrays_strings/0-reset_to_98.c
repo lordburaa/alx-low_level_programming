5060285f1b7f42ccfa5d7d2847640cd29eddef9b
lordburaa <comrdebiruk97@gmail.com>
2023-07-24 04:30:14 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+int _putchar(char a)
+{
+	return (write(1,&a,1));
+}
+void reset_to_98(int *n)
+{
+	*n = 98;
+ 
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef putcharr
+#define putcharr 
+
+int _putchar(char a);
+void reset_to_98(int *n);
+#endif
