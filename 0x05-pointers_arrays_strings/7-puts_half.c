f028c383aa1b6c2d4e94471c4621a376f2ce6917
lordburaa <comrdebiruk97@gmail.com>
2023-07-25 04:48:51 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - a function prins half of st
+ * @str: input
+ * Return: half
+ */
+void puts_half(char *str)
+{
+	int i, n, log;
+
+	log = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		log++;
+	n = (log / 2);
+	if ((log % 2) == 1)
+		n = ((log + 1) / 2);
+	for (i = n; str[i] != '\0';i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
