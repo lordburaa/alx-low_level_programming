1b77f5ed708c62c8d751b23f9414e5c8daff63d0
lordburaa <comrdebiruk97@gmail.com>
2023-07-24 22:00:10 -0700

    4-print_rev.c1

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main()
+{
+	char *str;
+	str = "I do not fear computers. Ifear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_rev - reverse string
+ * @s: char to be checked
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int i, m;
+	char j, d, f;
+
+	for (i = 0; i > -1; i++)
+	{
+		d = *(s + i);
+		if (d == '\0')
+			break;
+		j = j + 1;
+	}
+	for (m = i; m >= 0; m--)
+	{
+		i--;
+		f = *(s + i);
+		_putchar(f);
+		if (i == 0)
+		{
+			break;
+		}
+	}
+	_putchar('\n');
+}
