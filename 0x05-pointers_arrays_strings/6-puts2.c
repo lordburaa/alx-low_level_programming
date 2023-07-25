a77505ede616f64558365b8e3d0e31ab12742061
lordburaa <comrdebiruk97@gmail.com>
2023-07-25 01:47:22 -0700

    6-puts2.c2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,13 @@ void puts2(char *str)
 {
 	int i;
 	int j;
+
 	j = 0;
 	while (str[j] != '\0')
 	{
 		j++;
 	}
-
-	for (i = 0; i < j; i +=2)
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
 	}
