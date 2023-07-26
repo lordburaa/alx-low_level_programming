ef23e32956da6b3c98d006ac14a9d5e68ca84c55
lordburaa <comrdebiruk97@gmail.com>
2023-07-26 07:37:51 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - print number
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int k, i, j;
+
+	k = 0;
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (k < size)
+	{
+		j = size - k < 10 ? size - k : 10;
+		printf("%08x: ", 0);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + k + i));
+			else
+				printf(" ");
+
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int d = *(b + k + i);
+
+			if (d < 32 || d > 132)
+			{
+				d = '.';
+			}
+			printf("%c", d);
+		}
+		printf("\n");
+		k += 10;
+	}
+}
