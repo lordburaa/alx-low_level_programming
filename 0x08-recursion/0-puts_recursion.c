c4c1a47bc393b14d87b183ab82fcefd7e772b99d
lordburaa <comrdebiruk97@gmail.com>
2023-08-02 22:59:17 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string using recursion
+ * @s: char to be checked
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#ifndef main8
+#define main8
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
