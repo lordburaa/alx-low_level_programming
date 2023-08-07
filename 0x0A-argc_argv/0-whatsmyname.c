405a1c81a0bf112d1516f13a83ea267cc628f0f6
lordburaa <comrdebiruk97@gmail.com>
2023-08-07 01:20:00 -0700

    0-whatis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - name of excutable file
+ * @argc: number of arguments
+ * @argv: character
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[0]);
+	return 0;
+}
