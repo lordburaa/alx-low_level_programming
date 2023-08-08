f83ebb212a076bba49684b77820613ca02f82141
lordburaa <comrdebiruk97@gmail.com>
2023-08-08 06:27:25 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * str_concat - concatinate 2 string on allocated memory
+  * @s1: char to be checked
+  * @s2: char to be checked
+  * Return: Always succes
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k;
+
+	char *concat;
+
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		i++;
+	}
+	concat = malloc(sizeof(char) * i);
+	k = 0;
+	while (s1[j] != '\0')
+	{
+		concat[k] = s1[j];
+		k++;
+
+		j++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		concat[k] = s2[j];
+		k++;
+		j++;
+	}
+	return (concat);
+}
