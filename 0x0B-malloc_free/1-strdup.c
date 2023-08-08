7816cc169c1e10340471ede812850c8d7a1c3e95
lordburaa <comrdebiruk97@gmail.com>
2023-08-08 01:57:09 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,20 +1,29 @@
 #include<unistd.h>
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * _putchar - retun char
+ * @c: char to be checked
+ * Return: Always 0
+ */
 int _putchar(char c)
 {
-	return (write(1,&c,1));
-
+	return (write(1, &c, 1));
 }
+/**
+ * _strdup - char malloc to be used
+ * @str: char to be checked
+ * Return: always 0
+ */
 
 char *_strdup(char *str)
 {
 	int i;
 	char *ptr;
 	char c;
-	ptr = (char *)malloc(sizeof(str));
-	for (i = 0; str[i] != '\0';i++)
+
+	ptr = malloc(sizeof(str));
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		c = *(str + i);
 		_putchar(c);
