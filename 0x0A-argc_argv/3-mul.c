3cd296b6f897058f953a2a933ef65b93537a55d2
lordburaa <comrdebiruk97@gmail.com>
2023-08-09 04:26:25 -0700

    3-mult

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
+ * main - mult of two arg
+ * @argc: number of arg
+ * @argv: to be checked
+ * Return: Alwsys 0
  */
 int main(int argc, char *argv[])
 {
 	int mult;
+
 	if (argc == 3)
 	{
 	if (argv[1] && argv[2])
