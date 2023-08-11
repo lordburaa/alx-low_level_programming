0e701347feaa88e20cd8894c6ff0fb5608e71c74
lordburaa <comrdebiruk97@gmail.com>
2023-08-11 15:53:55 -0700

    all

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,10 +13,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-	for (idx = 0; s1[idx]; idx)
+	if (s2 == NULL)
+		s2 = "";
+	for (idx = 0; s1[idx]; idx++)
 		ln++;
 	con = malloc(sizeof(char) * (ln + 1));
-	if (con = NULL)
+	if (con == NULL)
 		return (NULL);
 	ln = 0;
 	for (idx = 0; s1[idx]; idx++)
@@ -24,5 +26,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (idx = 0; s2[idx] && idx < n; idx++)
 		con[ln++] = s2[idx];
 	con[ln] = '\0';
-	return (con):
+	return (con);
 }
