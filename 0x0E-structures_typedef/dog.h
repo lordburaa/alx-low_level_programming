e526f12b8d8b9666a8ec1e15505b4f14b8d761d5
lordburaa <comrdebiruk97@gmail.com>
2023-08-15 03:16:22 -0700

    0-main.c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef strucc
+#define strucc
+/**
+ * struct dog - owner
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return : 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
