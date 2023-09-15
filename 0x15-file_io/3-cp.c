0d3ca04491cc7b447f100a9f19645d6bd937fc00
lordburaa <comrdebiruk97@gmail.com>
2023-09-15 01:59:04 -0700

    coreected 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from the file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
