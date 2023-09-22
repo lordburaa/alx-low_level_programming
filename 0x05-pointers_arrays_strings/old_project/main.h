d8fd094671276fba5d01127a65ca5fbecafef14e
lordburaa <comrdebiruk97@gmail.com>
2023-09-21 23:01:42 -0700

    remove

--- 0x05-pointers_arrays_strings/old_project/main.h
+++ 0x05-pointers_arrays_strings/old_project/main.h
@@ -1,7 +1,11 @@
-#ifndef MAIN_5P
-#define MAIN_5p
+#ifndef putcharr
+#define putcharr 
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char a);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
@@ -9,5 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
 #endif
