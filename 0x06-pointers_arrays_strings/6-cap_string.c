cec05726553be523bf035e762f8d57cc353e8f5b
lordburaa <comrdebiruk97@gmail.com>
2023-09-25 00:56:57 -0700

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalize the first word
+ * @str: string to be chaecked
+ * Return: character
+ */
+
+char *cap_string(char *str)
+{
+	char c;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == ' '
+				|| str[i] == '\n' || str[i] == '!' || str[i] == '?'
+				|| str[i] == '\t' || str[i] == '"' || str[i] == '('
+				|| str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				c = str[i] - 32;
+				str[i] = c;
+				continue;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
