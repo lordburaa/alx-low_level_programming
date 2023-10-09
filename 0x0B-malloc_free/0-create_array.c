84d97b172cfcda2163b43b7564df8ffdc1fc6d93
lordburaa <comrdebiruk97@gmail.com>
2023-10-08 23:34:58 -0700

    0-create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,12 +17,10 @@ char *create_array(unsigned int size, char c)
 	}
 	ptr = (char*)malloc(sizeof(char) * size);
 
-	for (i = 0; i < size; i++)
-		ptr[i] = c;
-
 	if (ptr == NULL)
 		return (NULL);
-	free(ptr);
 	
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
 	return (ptr);
 }
