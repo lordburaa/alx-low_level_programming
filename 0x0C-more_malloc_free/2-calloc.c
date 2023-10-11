2da5a35cc2306494fed6c18773adf763ce1e547d
lordburaa <comrdebiruk97@gmail.com>
2023-10-11 04:57:59 -0700

    2-call

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,6 +10,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int i;
 
+	if (nmemb = 0 || size = 0)
+		return (NULL);
 	ptr = malloc(sizeof(size) * nmemb);
 	for (i = 0; i < nmemb; i++)
 		ptr[i] = 0;
