47f2af5cd60b04b054da08f87c40c2f2a40cbbe7
lordburaa <comrdebiruk97@gmail.com>
2023-10-12 17:28:30 -0700

    100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 
-	if (new_size >old_size)
+	if (new_size >old_size || ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
 		old_ptr = malloc(old_size);
