2b7ef170744163e7d35332eabb1676f54657351a
lordburaa <comrdebiruk97@gmail.com>
2023-10-19 04:06:37 -0700

    2-inti_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - array checking
+ * @array: array passed
+ * @size: sizeof  the array
+ * @cmp: function passed
+ * Return: the index cmp function found else 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= -1 || cmp == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
