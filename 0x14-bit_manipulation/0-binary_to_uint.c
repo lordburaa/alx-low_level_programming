57bf08af34151a8bd47aa32ff124e2cf9e9acece
lordburaa <comrdebiruk97@gmail.com>
2023-10-26 04:20:16 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,37 @@
 #include "main.h"
-
-unsigned int binary_to_unit(const char *b)
+/**
+ * binary_to_uint - change the binary to decimal
+ * @b: char containg bianry
+ *
+ * Return: if Success return converted binary else 0
+ */
+unsigned int binary_to_uint(const char *b)
 {
+	int i = 0;
+	int result = 1;
+	unsigned int sum = 0;
+	int check;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (((b[i] != '0') ^ (b[i] != '1')) == 0)
+			return (0);
+		i++;
+	}
+	i--;
+	check = i;
+	while (i >= 0)
+	{
+
+		if (b[i] == '1')
+		{
+			sum = result + sum;
+		}
+		result = 2 * result;
+		i--;
+	}
+	return (sum);
+
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_BIT
 #define MAIN_BIT
 
+/***********HEADER FILE *********/
+#include <stdio.h>
+
+
 unsigned int binary_to_uint(const char *b);
 #endif
