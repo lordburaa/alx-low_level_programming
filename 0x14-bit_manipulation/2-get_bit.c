892877d1d4f44b12740d2287ab7edfbebff8d2ce
lordburaa <comrdebiruk97@gmail.com>
2023-10-26 07:50:07 -0700

    4-clear_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int i = 0;
-	
+
 	if (index > 50)
 		return (-1);
 	i = ((n >> index) & 1);
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * clear_bit -  clear  bit at a given index
+ * @n: number to check
+ * @index: index to set
+ * Return: 1 if succes else 0
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int a;
+
+	if (index > 9)
+		return (0);
+	a = ~((index << 1));
+
+	*n = (a & (*n));
+
+	return (1);
+}
