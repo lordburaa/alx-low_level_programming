ee02a14dcafc88e13fdeb1aa11f306d1ef90a6b8
lordburaa <comrdebiruk97@gmail.com>
2023-10-26 17:42:38 -0700

    5-fli[

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,13 +3,13 @@
  * flip_bits - number of bits
  * @n: num one
  * @m: num two
- * Retrun: number of bits
+ * Return: number of bits
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int bit_s;
 
-	for(bit_s = 0; n || m; n >>= 1, m >>= 1)
+	for (bit_s = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			bit_s++;
