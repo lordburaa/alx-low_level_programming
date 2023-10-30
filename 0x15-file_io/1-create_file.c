dcbc261d4128cb0e18597a9b95eb2684f2ab1e18
lordburaa <comrdebiruk97@gmail.com>
2023-10-30 03:53:42 -0700

    1.6

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int wr;
 
-	fd = open(filename, O_RDWR | O_CREAT | O_APPEND, 00600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
 
 	if (fd == -1)
 		return (-1);
@@ -19,9 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[i] != '\0')
-			i++;
-	
-	
+			i++;	
 	}
 	wr = write(fd, text_content, i);
 	if (wr == -1)
