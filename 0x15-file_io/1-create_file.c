451ea5deec30179bd1674423fef6b01921345260
lordburaa <comrdebiruk97@gmail.com>
2023-10-30 03:46:10 -0700

    1.3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[i] != '\0')
 			i++;
-	text_content[i] = '\n';
-	i++;
+	
+	
 	}
 	write(fd, text_content, i);
 	close(fd);
