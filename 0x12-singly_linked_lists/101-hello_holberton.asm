d58fc2699fafc8ebd3d1e4df252571fb8c906b95
lordburaa <comrdebiruk97@gmail.com>
2023-11-29 06:53:07 -0800

    all

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,14 @@
-        global  main
-	 extern printf
-main:
-             move  edi, format
-	     xor   eax, eax
-	     call  printf
-	     mov   eax, 0
-	     ret
-format: db `Hello, Holberton\n`,0
+section .data
+	hello db 'Hello, Holberton', 0
+
+section .text
+	globla _start
+
+_start:
+	mov eax, 4
+	mov ebx, 2
+	mov ecx, hello
+	mov edx, 16
+	
+	mov eax, 1
+	xor ebx, ebx
