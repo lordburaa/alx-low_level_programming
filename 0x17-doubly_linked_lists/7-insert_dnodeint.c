b72733f499d950e59e7c873620b645643c2292d7
lordburaa <comrdebiruk97@gmail.com>
2023-12-07 04:07:22 -0800

    7-inset

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,6 +14,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	unsigned int k = 0;
 
+	if (*h == NULL)
+	{
+		return (NULL);
+	}
 	while (loop)
 	{
 		k = k + 1;
