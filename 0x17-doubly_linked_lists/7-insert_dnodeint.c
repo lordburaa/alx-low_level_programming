a43f57fa7b052ab654cd51ad2131d16c1ed1bd6e
lordburaa <comrdebiruk97@gmail.com>
2023-12-07 22:26:58 -0800

    all

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,8 +38,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->next = temp->next;
 			new->prev = temp;
 			
-			temp->next = new;
 			temp->next->prev = new;
+			temp->next = new;
 			return (new);
 
 		}
