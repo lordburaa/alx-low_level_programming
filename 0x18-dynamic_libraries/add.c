bcfcc3c9a7675d8af9e1ea184be2b1b86b5bda90
lordburaa <comrdebiruk97@gmail.com>
2023-12-11 03:10:22 -0800

    library for the operator

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,41 @@
+#include "list.h"
+/**
+ * add - add number
+ * @a: int a
+ * @b: int b
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - substraction
+ * @a: int a
+ * @b: in b
+ * Return: substraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplicatoin
+ * @a: int a
+ * @b: in b
+ * Retrun: multiplicaton of the number
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - modules
+ * @a: int a
+ * @b: in b
+ * Return: modules
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
