ab183d228e8063d1e6b9f1a591d2aa2c831d8b43
lordburaa <comrdebiruk97@gmail.com>
2023-12-21 22:48:17 -0800

    allocating for an arrya

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,13 +3,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i;
+
 	hash_table_t* new = malloc(sizeof(hash_table_t));
 
+	if (new == NULL)
+		return NULL;
+	new->size = size;
+	new->array = malloc(size * sizeof(hash_node_t *));
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		new->array[i] = NULL;
 	}
-	if (new == NULL)
 		return NULL;
 	return new;
 }
