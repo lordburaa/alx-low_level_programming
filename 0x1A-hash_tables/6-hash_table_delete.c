2057396bb70a9eecab9d2da5f979be8fcd3313f5
lordburaa <comrdebiruk97@gmail.com>
2023-12-24 00:24:41 -0800

    all

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
  * hash_table_delete - deletes hash table
- * @h" a pointer to hash table
+ * @ht: a pointer to hash table
  */
 void hash_table_delete(hash_table_t *ht)
 {
@@ -9,9 +9,9 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
-	for(i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if(ht->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
 			node = ht->array[i];
 			while (node != NULL)
